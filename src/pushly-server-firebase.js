fb1d2e5dc641ab59eee5e11f6a74f04721c1571f
krishnavamshi500apps <55684127+krishnavamshi500apps@users.noreply.github.com>
2019-11-14 10:58:36 +0530

    Update pushly-server-firebase.js

--- src/pushly-server-firebase.js
+++ src/pushly-server-firebase.js
@@ -26,18 +26,25 @@ export default class PushlyServerFirebase {
     static getPushlySideApproval() {
         // Popup Html content
         var c = `${window._pushGlobal.pushlyPopupHtml}`;
+        
         // Get all elements inside body tag
         var b = document.getElementsByTagName("BODY")[0];
+        
         // Create a div element inside body tag
         var a = document.createElement("div");
+        
         // Set id of div element added
         a.setAttribute("id", "myConsent");
+        
         // Add style to div element added
         a.setAttribute("style", "z-index: 10; overflow-x: overlay; overflow-y: overlay; top: 0; position: absolute; left: 0;");
+        
         // Add div element to body tag
         b.appendChild(a);
+        
         // Add popup Html to div elememt added
         document.getElementById("myConsent").innerHTML = c;
+        
         // Add event listener when allow button is clicked
         document.getElementById("_push_banner_allow").addEventListener("click", function () {
             // Send permission status
@@ -45,6 +52,7 @@ export default class PushlyServerFirebase {
             // Hide popup
             document.getElementById("myConsent").style.display = "none";
         });
+        
         // Add event listener when decline button is clicked
         document.getElementById("_push_banner_deny").addEventListener("click", function () {
             // Send permission status
@@ -79,6 +87,7 @@ export default class PushlyServerFirebase {
     static openChildWindow() {
         // Set localstorage for child window
         window.localStorage.setItem('_scb', 1);
+        
         // Open child window
         window._pushchildWindow = window.open(`${window._pushGlobal.serverUrl}/sw/` + window._push.apiKey, "Ratting", "width=550,height=500,left=150,top=200,toolbar=0,status=0,")
     }
